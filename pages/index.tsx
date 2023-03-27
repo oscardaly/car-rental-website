abf14aa9f5705bb2a10b7383185a522a2fcf8090
Caleb Wilson <calebwilson706@gmail.com>
2023-03-27 18:42:12 +0100

    Add carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
 import styles from "@/pages/index.module.css";
 import Head from "next/head";
+import ImageCarousel from "@/components/ImageCarousel";
+
+const getImages = () => {
+  // @ts-ignore
+  const context = require.context("public/homescreen-carousel", false);
+
+  const keys = context.keys();
+  return keys.map((key: string) => {
+    const fileName = key.slice(key.lastIndexOf("/") + 1);
+
+    return {
+      src: "homescreen-carousel" + "/" + fileName,
+      alt: fileName
+    };
+  });
+};
 
 export default function Home() {
+  const images = getImages();
+
   return (
-    <div className={styles.container}>
+    <div className={styles.index}>
       <Head>
-        <title>Page Title</title>
+        <title>NOS Rentals</title>
       </Head>
-      <h1>This is the home page</h1>
+      <ImageCarousel images={images}/>
     </div>
   );
 }
