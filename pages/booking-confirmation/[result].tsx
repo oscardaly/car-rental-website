d43764244c3a668b71038868ea8611a7c8992a30
Oscar Daly <oscardaly4@gmail.com>
2023-04-25 00:18:30 +0100

    Add url query params to booking confirmation page

--- pages/booking-confirmation/[result].tsx
+++ pages/booking-confirmation/[result].tsx
@@ -1,23 +1,33 @@
-import s from "@/pages/BookingConfirmation.module.css";
+import s from "@/pages/booking-confirmation/BookingConfirmation.module.css";
 import Head from "next/head";
 import React, {FC} from "react";
 import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
+import {useRouter} from "next/router";
 
-interface BookingConfirmationProps {
-  success: boolean,
-  imagePath?: string,
-  leftDescription?: string,
-}
-
-const BookingConfirmation: FC<BookingConfirmationProps> = ({success, imagePath, leftDescription}) => {
-  const title: string = success ? "Thank you for booking!" : "Booking Failed";
-  leftDescription = success ? leftDescription : "Unfortunately, your booking could not be processed, please try again or contact us " +
-    "if this is an issue that frequently persists.";
+const Result: FC = () => {
+  const router = useRouter();
+  const {result, carId} = router.query;
+  const success = result === "success";
+  let title: string;
+  let leftDescription: string;
+  let imagePath: string = "";
   const rightDescription: string = "If you have any questions, concerns or feedback, please email us at: info@nosrentals.com or " +
     "contact us on one of our social channels and we'll be more than happy to help. Thank you!";
   const {width, height} = useWindowSize();
 
+  if (success) {
+    title = "Thank you for booking!";
+    const car: Car = getCarById(carId);
+    leftDescription = car.getDescription;
+    imagePath = car.getImagePath;
+  }
+
+  else {
+    title = "Booking Failed";
+    leftDescription = "Unfortunately, your booking could not be processed, please try again or contact us " +
+      "if this is an issue that frequently persists.";
+  }
 
   return (
     <div className={s.container}>
@@ -26,7 +36,7 @@ const BookingConfirmation: FC<BookingConfirmationProps> = ({success, imagePath, 
       </Head>
       <div className={s.header}>
         <h1>{title}</h1>
-        {success &&
+        { success &&
           <div>
             <Confetti
               width={width}
@@ -60,4 +70,4 @@ const BookingConfirmation: FC<BookingConfirmationProps> = ({success, imagePath, 
   );
 };
 
-export default BookingConfirmation;
+export default Result;
